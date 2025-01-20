5ab2ffb45baa78e648111a9594656b73f0b61204
= <=>
2025-01-20 21:49:49 +0100

    add quote.json file

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <!-- tamplate -->
 <template>
   <h1>Liste de citations !</h1>
+
+  <p>Voici la citation du jour</p>
+  {{ citation }}
 </template>
 
 <!-- script -->
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+let url = "../public/quote.json";
+
+let citation = ref("");
+let data = ref("");
+
+// fecth
+
+fetch(url)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error("Network response was not ok.");
+  })
+  .then((data) => {
+    citation.value = data;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+</script>
 
 <!-- style -->
 <style></style>
